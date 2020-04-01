b98f3ca3cfd549753557ac91e1fb62d80161d7e1
wangziyingwen <wz.lxh@outlook.com>
2020-04-01 19:17:48 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -38,39 +38,40 @@ def main():
     'Content-Type':'application/json'
     }
     print('账号 '+str(a)+' 此次运行开始时间为 :', localtime)
+    print(repr(num1))
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print("1调用成功"+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print("2调用成功"+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('3调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('4调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('5调用成功'+str(num1)+'次')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('6调用成功'+str(num1)+'次')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('7调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('8调用成功'+str(num1)+'次')
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('8调用成功'+str(num1)+'次') 
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('9调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
-            num1[a]+=1
+            num1[a]=num1[a]+1
             print('10调用成功'+str(num1)+'次')
     except:
         print("pass")
