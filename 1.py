e3ca40f47c9139ddd4f1adf10e8b12bad38bdabd
wangziyingwen <wz.lxh@outlook.com>
2020-04-01 18:32:17 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -10,7 +10,8 @@ import json,sys,time,random
 
 id_list = [r'b2b60c8e-e838-4a86-94a6-1b5f689028b0',r'OldV=42tW9l1/FYuzB.sipZmo@dnFKHc']
 secret_list = [r'9a587110-8249-4f22-a686-e11b553f5f6e',r'.alg52tc5:DDhL4Dmn/lDzJIeupgyBV.']
-
+config_list = {'每次轮数':6,'是否启动随机时间':'Y','延时范围起始':600,'结束':1200}
+    
 num1 = [0]*len(id_list)
 def gettoken(refresh_token):
     headers={'Content-Type':'application/x-www-form-urlencoded'
@@ -27,16 +28,62 @@ def gettoken(refresh_token):
     access_token = jsontxt['access_token']
     return access_token
 def main():
-    print(a)
     fo = open(path, "r+")
     refresh_token = fo.read()
     fo.close()
     localtime = time.asctime( time.localtime(time.time()) )
-    print('开始'+localtime)
-    print(refresh_token)
-for _ in range(3): 
-    b=random.randint(5,10)
-    time.sleep(b)
-    for a in range(0, len(id_list)):
-	    path=sys.path[0]+r'/'+str(a)+'.txt'
-	    main()
+    access_token=gettoken(refresh_token)
+    headers={
+    'Authorization':access_token,
+    'Content-Type':'application/json'
+    }
+    print('账号 '+str(a)+' 此次运行开始时间为 :', localtime)
+    try:
+        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
+            num1+=1
+            print("1调用成功"+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
+            num1+=1
+            print("2调用成功"+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
+            num1+=1
+            print('3调用成功'+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
+            num1+=1
+            print('4调用成功'+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
+            num1+=1
+            print('5调用成功'+str(num1)+'次')    
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
+            num1+=1
+            print('6调用成功'+str(num1)+'次')    
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
+            num1+=1
+            print('7调用成功'+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
+            num1+=1
+            print('8调用成功'+str(num1)+'次')
+        if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
+            num1+=1
+            print('8调用成功'+str(num1)+'次') 
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
+            num1+=1
+            print('9调用成功'+str(num1)+'次')
+        if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
+            num1+=1
+            print('10调用成功'+str(num1)+'次')
+    except:
+        print("pass")
+        pass
+if config_list['是否启动随机时间'] == 'Y':        
+    for _ in range(config_list['每次轮数']): 
+        b=random.randint(config_list['延时范围起始'],config_list['结束'])
+        time.sleep(b)
+        for a in range(0, len(id_list)):
+	        path=sys.path[0]+r'/'+str(a)+'.txt'
+	        main()
+else:
+    for _ in range(config_list['每次轮数']): 
+        for a in range(0, len(id_list)):
+	        path=sys.path[0]+r'/'+str(a)+'.txt'
+	        main()
