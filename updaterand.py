690b9a014a6aaf016757e2e3388e9511dd08943e
wangziyingwen <wz.lxh@outlook.com>
2020-04-02 18:20:35 +0800

    Update updaterand.py

--- updaterand.py
+++ updaterand.py
@@ -8,8 +8,23 @@ import json,sys,time,random
 #注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
 #以下空行不要删除，以便运行时插入机密
 
-randomapi=[1,2,3,4,5,6,7,8,9,10]
+slice1 = [0]*4
+randomapi=[1,2,6,7,21,22]
+list1 = [[3,4,5],[8,9,10],[23,24,25,26,27,28],[14,15,16,17],[18,19,20]]
+list2 = [1,2,2,2,2]
 path=sys.path[0]+r'/randomapi.txt'
+
+for i in range(0,3):
+    slice1[i] = random.sample(list1[i], list2[i])
+b = random.randint(0,3)
+if b == 0 :
+    slice1[3]=[12,13]
+if b == 1 :
+    slice1[3]=random.sample(list1[3], list2[3])
+if b == 2 :
+    slice1[3]=random.sample(list1[4], list2[4])
+for a in range(0,4):
+    randomapi.append(slice1[a])
 
 random.shuffle(randomapi)
 str2 = ','.join([str(x) for x in randomapi])
